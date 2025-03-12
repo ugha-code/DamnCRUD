92aca850dec782b81be579508b8c1068295f352c
ugha-code <u.gha523@gmail.com>
2025-03-12 10:13:17 +0700

    Update functions.php

--- functions.php
+++ functions.php
@@ -3,7 +3,7 @@ function pdo_connect(){
     $DATABASE_HOST = 'localhost';
     $DATABASE_USER = 'root';
     $DATABASE_PASS = '';
-    $DATABASE_NAME = 'damncrud';
+    $DATABASE_NAME = 'damncrud'; 
     try {
         return new PDO('mysql:host=' . $DATABASE_HOST . ';dbname=' . $DATABASE_NAME, $DATABASE_USER, $DATABASE_PASS);
     } catch (PDOException $exception) {
