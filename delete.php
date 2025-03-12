c7f925c666aa7b24947107dff0475ea705475f15
ugha-code <u.gha523@gmail.com>
2025-03-12 10:44:40 +0700

    Update delete.php

--- delete.php
+++ delete.php
@@ -1,17 +1,18 @@
 <?php
 session_start();
 if (!isset($_SESSION['user'])) {
-    header("location: login.php");
-} else {
-    include 'functions.php';
-    $pdo = pdo_connect();
+    header("Location: login.php");
+    exit;
+}
+include 'functions.php';
+$pdo = pdo_connect();
 
-    if (isset($_GET['id'])) {
-        $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
-        $stmt->execute([$_GET['id']]);
-        header("location:index.php");
-    } else {
-        die ('No ID specified!');
-    }
-} 
+if (isset($_GET['id'])) {
+    $stmt = $pdo->prepare('DELETE FROM contacts WHERE id = ?');
+    $stmt->execute([$_GET['id']]);
+    header("Location: index.php");
+    exit;
+} else {
+    die("No ID specified!");
+}
 ?>
