a133c1969dd91822429e24f3d9453273bb9ee783
ugha-code <u.gha523@gmail.com>
2025-03-12 10:45:48 +0700

    Update menu.php

--- menu.php
+++ menu.php
@@ -1,10 +1,10 @@
 <div class="row">
     <div class="col">
-        <a type="button" class="btn btn-success create-contact" href="create.php">Add New Contact</a>
+        <a class="btn btn-success create-contact" href="create.php">Add New Contact</a>
         <div class="action float-right">
-        <a type="button" class="btn btn-primary" href="profil.php">Profil</a> 
-        <a type="button" class="btn btn-warning" href="vpage.php">VPage</a> 
-        <a type="button" class="btn btn-danger" href="logout.php">Sign out</a> 
+            <a class="btn btn-primary" href="profil.php">Profil</a> 
+            <a class="btn btn-warning" href="vpage.php">VPage</a> 
+            <a class="btn btn-danger" href="logout.php">Sign out</a> 
         </div>                  
     </div>
 </div>
