eace58cb835d2664457ed12fdc49f9af6ff0cedd
ugha-code <u.gha523@gmail.com>
2025-03-12 10:45:28 +0700

    Update logout.php

--- logout.php
+++ logout.php
@@ -1,9 +1,11 @@
-<?php 
-    session_start();
-    if (!isset($_SESSION['user'])) {
-        header("location: login.php");
-    } else {
-        session_destroy();
-        header('location:login.php');
-    }
+<?php
+session_start();
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit;
+} else {
+    session_destroy();
+    header("Location: login.php");
+    exit;
+}
 ?>
